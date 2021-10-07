4f64159683aedf2fb0dfa3532a85f8f564cb7c6b
Not-complet <77133545+Not-complet@users.noreply.github.com>
2021-10-06 21:33:31 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    video.position(300,100)
+
+    canvas = createCanvas(600,600);
+    canvas.position(300,500);
     
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw(){
+    background("#a19b9a")
+}
+function modelLoaded(){
+    console.log("PoseNet Loaded");
+}
+function gotPoses(results){
+    if(results.length > 0 ){
+        console.log(results);
+    }
 }
