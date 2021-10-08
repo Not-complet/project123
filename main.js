40dd2ae9413e8e2ada2013d120dbc2b64dfbe345
Not-complet <77133545+Not-complet@users.noreply.github.com>
2021-10-07 21:13:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
     video.position(300,100)
 
     canvas = createCanvas(600,600);
-    canvas.position(300,500);
+    canvas.position(300,550);
     
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose',gotPoses);
 }
 function draw(){
-    background("#a19b9a")
+    background("#a19b9a");
+    textSize(difference);
+    fill("#26abed");
+    text("Miguel", 100, 200);
 }
 function modelLoaded(){
     console.log("PoseNet Loaded");
@@ -18,5 +24,8 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0 ){
         console.log(results);
+        leftWristX = floor(results[0].pose.leftWrist.x);
+        rightWristX = floor(results[0].pose.rightWrist.x);
+        difference = leftWristX - rightWristX;
     }
 }
